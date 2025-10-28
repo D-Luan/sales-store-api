9b8673fc3c7598ad90c6f6741cee475637abef12
d-luan <dluan1285@gmail.com>
2025-10-28 10:44:38 -0300

    feat(Controllers): add ProductsController

---
+++ src/SalesStore.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesStore.WebAPI.Data;
+
+namespace SalesStore.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly SalesStoreDbContext _context;
+
+    public ProductsController(SalesStoreDbContext context)
+    {
+        _context = context;
+    }
+}
