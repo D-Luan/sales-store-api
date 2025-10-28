fe0dbb2ded1d6be6cff0d302e8adc03e88833e02
d-luan <dluan1285@gmail.com>
2025-10-28 10:31:21 -0300

    feat(Models): add Product model to the project

---
+++ src/SalesStore.WebAPI/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace SalesStore.WebAPI.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public int Quantity { get; set; }
+
+    public string Category { get; set; } = string.Empty;
+}
