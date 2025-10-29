80c3fe1e368d7d64f47ac788b02e8b3c5b10ac9e
d-luan <dluan1285@gmail.com>
2025-10-29 09:12:13 -0300

    feat(ProductsController): add GetProductByIdAsync endpoint

--- src/SalesStore.WebAPI/Controllers/ProductsController.cs
+++ src/SalesStore.WebAPI/Controllers/ProductsController.cs
@@ -32,4 +32,17 @@ public class ProductsController : ControllerBase
 
         return Ok(products);
     }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProductByIdAsync(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(product);
+    }
 }
