30dc881a9b032735c84f7236a3d3eabcc95da3df
d-luan <dluan1285@gmail.com>
2025-10-29 10:20:51 -0300

    feat(ProductsController): add DeleteProductAsync endpoint

--- src/SalesStore.WebAPI/Controllers/ProductsController.cs
+++ src/SalesStore.WebAPI/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using SalesStore.WebAPI.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 namespace SalesStore.WebAPI.Controllers;
 
@@ -61,6 +62,22 @@ public class ProductsController : ControllerBase
         product.Quantity = newProduct.Quantity;
         product.Category = newProduct.Category;
 
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProductAsync(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        _context.Products.Remove(product);
         await _context.SaveChangesAsync();
 
         return NoContent();
